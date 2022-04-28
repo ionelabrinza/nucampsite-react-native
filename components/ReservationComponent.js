ff74d46fea2a9de41e60f8e3109912e582ed6d33
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-04-28 01:13:19 -0500

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,8 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
+
 
 
 
@@ -37,7 +39,10 @@ class Reservation extends Component {
                         },
                         {
                         text: 'OK',
-                        onPress: () => this.resetForm()
+                        onPress: () => {
+                            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                            this.resetForm(); 
+                        }
                         },
                     ],
                     { cancelable : false }
@@ -52,6 +57,31 @@ class Reservation extends Component {
             date: new Date(),
             showCalendar: false,
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
     
 
