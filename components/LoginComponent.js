657f4aeab0da1ebdd84c13a3b60424523aa54415
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-04-30 22:33:54 -0500

    Week 4 assignment

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 
 
@@ -161,10 +162,43 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
             }
         }
     }
+
+
+//Workshop week 4, task 2
+
+    getImageFromGallery = async () => {
+        
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
+
+// Workshop week 4, task 1 
+    processImage = async (imgUri) => {
+       const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri,
+      [
+        { resize: {width: 400}}
+      ],
+      { format: 'png' }
+    );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
+
+    };
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
@@ -196,6 +230,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                 <Input
